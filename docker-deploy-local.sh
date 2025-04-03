91c8941ad4d3a9b602b085ad1152c4352f09980d
ekrombouts <ekrombouts@gmail.com>
2025-04-03 09:46:59 +0200

    under construction, but running!

--- docker-deploy-local.sh
+++ docker-deploy-local.sh
@@ -1,24 +1,10 @@
-#!/bin/bash
-
-# Variabelen instellen
-RESOURCE_GROUP="rg-gardenia-app"
-KEYVAULT_NAME="kv-gardenia"
-IMAGE_NAME="gardenia:v1.0-local"
-CONTAINER_NAME=gardenia-app-v1.0
-DB_HOST="gardenia-db-container"
-DB_NAME="gardenia"
-DB_USER="gardenia"
-DB_PORT="5432" # Default 5432
-APP_PORT="8501"  # Default 8501 - Voor de lokale productie omgeving
-
 # Wachtwoorden ophalen uit Key Vault
-DB_PASSWORD=$(az keyvault secret show --vault-name $KEYVAULT_NAME --name DB-PASS --query value -o tsv)
+DB_PASSWORD=$(az keyvault secret show --vault-name "kv-gardenia" --name DB-PASS --query value -o tsv)
 
-docker-compose down --remove-orphans
+# Stop en verwijder de huidige containers
+docker-compose down --remove-orphans 
 docker system prune -af
-export IMAGE_NAME DB_HOST DB_NAME DB_USER DB_PORT DB_PASSWORD APP_PORT CONTAINER_NAME
+
+export DB_PASSWORD 
 docker-compose build --no-cache
 docker-compose up -d
-
-echo "Je Gardenia app is lokaal beschikbaar op:"
-echo "http://localhost:$APP_PORT"
